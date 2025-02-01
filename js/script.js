61003c32be022895408da5122ffce8393a622ad8
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-02-01 17:34:05 +0000

    syntax error and freeze of vs code
    Co-authored-by: SJECollins <SJECollins@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,33 +1,17 @@
-const API_KEY = "C7g8-wBc02FgxzEYXhOzC9CDmy4";
+const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+}
 
 async function getStatus(e) {
+}
 
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
-
-    const response = await fetch(queryString);
-
-    const data = await response.json();
-
-    if (response.ok) {
-        displayStatus(data);
-    } else {
-        throw new Error(data.error);
-    }
-
+function displayErrors(data) {
 }
 
 function displayStatus(data) {
-
-    let heading = "API Key Status";
-    let results = `<div>Your key is valid until</div>`;
-    results += `<div class="key-status">${data.expiry}</div>`;
-
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
-
-}
